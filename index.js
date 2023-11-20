2bd34eb63a8011b9e95e47a815e02c4602793a8c
jennyweic <weicchan@gmail.com>
2023-11-20 16:29:33 +0000

    Call function to update time every min

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-//London city details
-let londonCityElement = document.querySelector("#london-city");
-let londonCityDateElement = londonCityElement.querySelector(".date");
-let londonCityTimeElement = londonCityElement.querySelector(".time");
-let londonCityTime = moment().tz("Europe / London");
+// call this into a function to run every minute
+function showCityClock() {
+  //London city details
+  let londonCityElement = document.querySelector("#london-city");
+  let londonCityDateElement = londonCityElement.querySelector(".date");
+  let londonCityTimeElement = londonCityElement.querySelector(".time");
+  let londonCityTime = moment().tz("Europe / London");
 
-londonCityDateElement.innerHTML = londonCityTime.format("dddd Do MMMM YYYY");
+  londonCityDateElement.innerHTML = londonCityTime.format("dddd Do MMMM YYYY");
 
-londonCityTimeElement.innerHTML = londonCityTime.format(
-  "HH:mm [<span>]A[</span>]"
-);
+  londonCityTimeElement.innerHTML = londonCityTime.format(
+    "HH:mm [<span>]A[</span>]"
+  );
+}
+setInterval(showCityClock, 1000);
