1f26a737904479a1d513e7add27266c5b7307707
jennyweic <weicchan@gmail.com>
2023-11-20 17:40:13 +0000

    Comment out long form for city details

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-// call this into a function to run every minute
-function showCityClock() {
-  //London city details
-  let londonCityElement = document.querySelector("#london-city");
-  let londonCityDateElement = londonCityElement.querySelector(".date");
-  let londonCityTimeElement = londonCityElement.querySelector(".time");
-  let londonCityTime = moment().tz("Europe/London");
+// // call this into a function to run every minute
+// function showCityClock() {
+//   //London city details
+//   let londonCityElement = document.querySelector("#london-city");
+//   let londonCityDateElement = londonCityElement.querySelector(".date");
+//   let londonCityTimeElement = londonCityElement.querySelector(".time");
+//   let londonCityTime = moment().tz("Europe/London");
 
-  londonCityDateElement.innerHTML = londonCityTime.format("dddd Do MMMM YYYY");
+//   londonCityDateElement.innerHTML = londonCityTime.format("dddd Do MMMM YYYY");
 
-  londonCityTimeElement.innerHTML = londonCityTime.format(
-    "HH:mm [<span>]A[</span>]"
-  );
+//   londonCityTimeElement.innerHTML = londonCityTime.format(
+//     "HH:mm [<span>]A[</span>]"
+//   );
 
-  //Hong Kong city details
-  let hongKongCityElement = document.querySelector("#hk-city");
-  let hongKongCityDateElement = hongKongCityElement.querySelector(".date");
-  let hongKongCityTimeElement = hongKongCityElement.querySelector(".time");
-  let hongKongCityTime = moment().tz("Asia/Hong_Kong");
+//   //Hong Kong city details
+//   let hongKongCityElement = document.querySelector("#hk-city");
+//   let hongKongCityDateElement = hongKongCityElement.querySelector(".date");
+//   let hongKongCityTimeElement = hongKongCityElement.querySelector(".time");
+//   let hongKongCityTime = moment().tz("Asia/Hong_Kong");
 
-  hongKongCityDateElement.innerHTML =
-    hongKongCityTime.format("dddd Do MMMM YYYY");
+//   hongKongCityDateElement.innerHTML =
+//     hongKongCityTime.format("dddd Do MMMM YYYY");
 
-  hongKongCityTimeElement.innerHTML = hongKongCityTime.format(
-    "HH:mm [<span>]A[</span>]"
-  );
+//   hongKongCityTimeElement.innerHTML = hongKongCityTime.format(
+//     "HH:mm [<span>]A[</span>]"
+//   );
 
-  //Portugal city details
-  let portugalCityElement = document.querySelector("#portugal-city");
-  let portugalCityDateElement = portugalCityElement.querySelector(".date");
-  let portugalCityTimeElement = portugalCityElement.querySelector(".time");
-  let portugalCityTime = moment().tz("Europe/Lisbon");
+//   //Portugal city details
+//   let portugalCityElement = document.querySelector("#portugal-city");
+//   let portugalCityDateElement = portugalCityElement.querySelector(".date");
+//   let portugalCityTimeElement = portugalCityElement.querySelector(".time");
+//   let portugalCityTime = moment().tz("Europe/Lisbon");
 
-  portugalCityDateElement.innerHTML =
-    portugalCityTime.format("dddd Do MMMM YYYY");
+//   portugalCityDateElement.innerHTML =
+//     portugalCityTime.format("dddd Do MMMM YYYY");
 
-  portugalCityTimeElement.innerHTML = portugalCityTime.format(
-    "HH:mm [<span>]A[</span>]"
-  );
+//   portugalCityTimeElement.innerHTML = portugalCityTime.format(
+//     "HH:mm [<span>]A[</span>]"
+//   );
 
-  //Oslo city details
-  let osloCityElement = document.querySelector("#oslo-city");
-  let osloCityDateElement = osloCityElement.querySelector(".date");
-  let osloCityTimeElement = osloCityElement.querySelector(".time");
-  let osloCityTime = moment().tz("Europe/Oslo");
+//   //Oslo city details
+//   let osloCityElement = document.querySelector("#oslo-city");
+//   let osloCityDateElement = osloCityElement.querySelector(".date");
+//   let osloCityTimeElement = osloCityElement.querySelector(".time");
+//   let osloCityTime = moment().tz("Europe/Oslo");
 
-  osloCityDateElement.innerHTML = osloCityTime.format("dddd Do MMMM YYYY");
+//   osloCityDateElement.innerHTML = osloCityTime.format("dddd Do MMMM YYYY");
 
-  osloCityTimeElement.innerHTML = osloCityTime.format(
-    "HH:mm [<span>]A[</span>]"
-  );
-}
-setInterval(showCityClock, 1000);
+//   osloCityTimeElement.innerHTML = osloCityTime.format(
+//     "HH:mm [<span>]A[</span>]"
+//   );
+// }
+// setInterval(showCityClock, 1000);
 
 // create a function to update multiple locations
 function updateCity(event) {
